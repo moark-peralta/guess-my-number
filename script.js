df875fd71ed855d66e1f368fdf9c6b2eac05f150
moeark-peralta <markperaltaasdgit@gmail.com>
2024-02-14 20:30:38 +0800

    again button logic

--- script.js
+++ script.js
@@ -9,16 +9,22 @@ const number = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.number').textContent = number;
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // WHEN NO INPUT
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number 💔';
+
+    // WHEN PLAYER WINS
   } else if (guess === number) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = number;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    // WHEN GUESS IS TOO HIGH
   } else if (guess > number) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High!';
@@ -28,6 +34,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game :(';
       document.querySelector('.score').textContent = 0;
     }
+    // WHEN GUESS IS TOO LOW
   } else if (guess < number) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low!';
@@ -38,4 +45,13 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
